9d92a7dd690075e45b79d316b8c4494a82d9d7c4
nsst <nsst@itu.dk>
2020-04-13 18:20:39 +0200

    good progress

--- oracle.py
+++ oracle.py
@@ -3,7 +3,7 @@
 
 import sys
 
-p = [[], [], [], []]
+p = [[], [], [], [], []]
 
 lines = sys.stdin.readlines()
 lemma = sys.argv[1]
@@ -11,18 +11,11 @@ lemma = sys.argv[1]
 for line in lines:
     num = line.split(':')[0]
 
-    # if lemma == 'paper_accepted_verifiability_soundness_1_exists_trace_where_false':
-    #     if any(string in line for string in ['!PC(']):
-    #         p[0].append(num)
-    #     elif any(string in line for string in ['Ready_']):
-    #         p[1].append(num)
-    #     elif any(string in line for string in ['!PC', 'Make']):
-    #         p[2].append(num)
-    #     else:
-    #         p[3].append(num)
+    if any(string in line for string in ['pc_privk']):
+        p[0].append(num)
 
-    # else:
-    p[3].append(num)
+    else:
+        p[3].append(num)
 
 
 for goal in [item for sublist in p for item in sublist]:
