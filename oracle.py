2b3816ed0b54257a2601ec295bc93e3a9690443c
nsst <nsst@itu.dk>
2020-04-17 16:31:23 +0200

    Friday! :D

--- oracle.py
+++ oracle.py
@@ -8,15 +8,30 @@ p = [[], [], [], [], []]
 lines = sys.stdin.readlines()
 lemma = sys.argv[1]
 
+excluded_lemmas = [
+    'paper_accepted_verifiability_soundness_2_true_no_adversary',
+    'paper_rejected_verifiability_soundness_2_true_no_adversary',
+    'key_secret',
+    'paper_secrecy_accepted',
+    'paper_secrecy_rejected',
+    'review_secrecy',
+]
+
+
 for line in lines:
     num = line.split(':')[0]
-
-    if any(string in line for string in ['pc_privk']):
-        p[0].append(num)
-
+    if lemma not in excluded_lemmas:
+        if 'pc_privk' in line:
+            p[0].append(num)
+        else:
+            p[3].append(num)
+    elif 'paper_secrecy_rejected' in lemma:
+        if 'privk' in line:
+            p[0].append(num)
+        else:
+            p[3].append(num)
     else:
-        p[3].append(num)
-
+        p[0].append(num)
 
 for goal in [item for sublist in p for item in sublist]:
     print(goal)
